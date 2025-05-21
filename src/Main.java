c9654f7abaffa06483f9b7e126899e55774c0eb7
KamilR899 <s33495@pjwstk.edu.pl>
2025-05-21 14:05:11 +0200

    Zad 1/2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Utworzenie instancji klasy Car
-        Car myCar = new Car(5); // Załóżmy, że samochód ma 5 miejsc
 
-        // Wywołanie przesłoniętych metod start() i stop() z klasy Car
-        myCar.start(); // Wyświetli: Samochód uruchamia silnik.
-        myCar.stop();  // Wyświetli: Samochód gasi silnik.
+        Car myCar = new Car(5);
 
 
-        // Dla porównania, jeśli mielibyśmy obiekt Vehicle:
+        myCar.start();
+        myCar.stop();
+
+
         Vehicle Fiat = new Vehicle();
         Fiat.start();
         Fiat.stop();
